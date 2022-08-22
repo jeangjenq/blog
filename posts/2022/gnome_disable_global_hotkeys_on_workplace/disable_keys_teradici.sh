b6143a0cd478bf5f10e2ffe9909038ad48ea5e42
jeangjenq <jeangjenq@hotmail.com>
2022-08-22 22:59:24 +1200

    gnome remote hotkeys workflow

---
+++ posts/2022/gnome_disable_global_hotkeys_on_workplace/disable_keys_teradici.sh
@@ -0,0 +1,10 @@
+xdotool set_desktop --relative 1
+gsettings set org.gnome.desktop.wm.keybindings switch-applications []
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward []
+gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-left []
+gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-right []
+gsettings set org.gnome.desktop.wm.keybindings maximize []
+gsettings set org.gnome.desktop.wm.preferences mouse-button-modifier ''
+gsettings set org.gnome.mutter.keybindings toggle-tiled-left []
+gsettings set org.gnome.mutter.keybindings toggle-tiled-right []
+gsettings set org.gnome.mutter overlay-key ''
---
+++ posts/2022/gnome_disable_global_hotkeys_on_workplace/enable_keys_home.sh
@@ -0,0 +1,10 @@
+xdotool set_desktop --relative -- -1
+gsettings reset org.gnome.desktop.wm.keybindings switch-applications
+gsettings reset org.gnome.desktop.wm.keybindings switch-applications-backward
+gsettings reset org.gnome.desktop.wm.keybindings move-to-monitor-left
+gsettings reset org.gnome.desktop.wm.keybindings move-to-monitor-right
+gsettings reset org.gnome.desktop.wm.keybindings maximize
+gsettings reset org.gnome.desktop.wm.preferences mouse-button-modifier
+gsettings reset org.gnome.mutter.keybindings toggle-tiled-left
+gsettings reset org.gnome.mutter.keybindings toggle-tiled-right
+gsettings reset org.gnome.mutter overlay-key
