c1010798ddd05bcc35eac655f8d5ee00382ed941
jeangjenq <jeangjenq@worf>
2023-02-06 22:23:53 +1300

    more detail in regex and better glob search

--- posts/2023/find_all_versions.py
+++ posts/2023/find_all_versions.py
@@ -1,33 +1,35 @@
 import re
 from glob import glob
 
-def detect_all_versions(path):
-    # detect all versions that exists in node folder
-    # nukescripts.version_latest() contains similar function
-    # but it's only friendly with v+1
-    # so if there's a version skip it stops at a certain version
-    # not my prefer method
+def detect_all_versions(self, node):
+    '''
+    detect all versions that exists in node folder
+    nukescripts.version_latest() contains similar function
+    but it's only friendly with v+1
+    so if there's a version skip it stops at a certain version
+    not my prefer method
+    '''
     versions = []
+    file = nuke.filename(node).replace('\\', '/')
     # regex patterns to replace versions and frame paddings to *
     # we will use glob later to search for all files
-    versions_pattern = [r"v-?\d+"]
+    versions_pattern = r"(?P<symbol>[\._\\/])(?P<prefix>[vV])(?P<num>-?\d+)"
     frame_paddings = [r"%\d+[dD]", r"\#+"]
 
-    for pattern in versions_pattern + frame_paddings:
-        path = re.sub(pattern, "*", path)
+    file = re.sub(versions_pattern, "\g<symbol>\g<prefix>[0-9]*", file)
+    for pattern in frame_paddings:
+        file = re.sub(pattern, "[0-9]*", file)
+    # example of file at this point
+    #  /media/checker_v[0-9]*/checker_v[0-9]*.[0-9]*.exr
     
-    # make sure all v### found in files are the same number
-    # don't know under what occasion you might have 2 different versions
-    # in same path, don't know what to do about it
-    def all_same(items):
-        return all(x == items[0] for x in items)
-    # glob returns list of files that matches the pattern
+    # glob  will returns list of files that matches the pattern
     # in this case probably all the individual frames
-    files = glob(path)
+    files = glob(file)
     versions = []
-    for path in files:
-        find_version = re.findall(versions_pattern[0], path)
-        if find_version and all_same(find_version):
-            if find_version[0] not in versions:
-                versions.append(find_version[0])
+    for file in files:
+        find_version = re.search(versions_pattern, file)
+        if find_version:
+            version = find_version.group('prefix') + find_version.group('num')
+            if version not in versions:
+                versions.append(version)
     return versions
